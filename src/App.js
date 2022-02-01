c991afbe5c8f83217b62cac61e1eb690102feb80
danbe <danbe8080@gmail.com>
2022-02-01 18:29:12 +0900

    react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
